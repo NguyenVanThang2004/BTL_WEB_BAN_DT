1a98a6df7d9a3780a7a116357efa71fb7b84fbfc
NguyenVanThang2004 <154830941+NguyenVanThang2004@users.noreply.github.com>
2024-10-14 15:55:04 +0700

    1. Layout index , Layout Admin

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_WEB_BAN_DT.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        
+
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
